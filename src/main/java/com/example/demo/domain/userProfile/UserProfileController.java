2a744aceb9845db3fa5bdf0601b1cac44d400db0
nuweramohammad <mohammad@noseryoung.com>
2021-11-10 10:15:03 +0100

    checked authorities endpoints after adding data to database with shawn

--- src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
+++ src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
@@ -1,5 +1,41 @@
 package com.example.demo.domain.userProfile;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/userprofile")
+@RequiredArgsConstructor
 public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    @GetMapping("/read")
+    @PreAuthorize("hasAuthority('READ')")
+    public ResponseEntity<String> TestRead() {
+        return ResponseEntity.ok().body("You Can Read This Page");
+    }
+
+    @GetMapping("/create")
+    @PreAuthorize("hasAuthority('CREATE')")
+    public ResponseEntity<String> TestCreate() {
+        return ResponseEntity.ok().body("You Can Create On This Page");
+    }
+
+    @GetMapping("/delete")
+    @PreAuthorize("hasAuthority('DELETE')")
+    public ResponseEntity<String> TestDelete() {
+        return ResponseEntity.ok().body("You Can Delete On This Page");
+    }
+
+    @GetMapping("/update")
+    @PreAuthorize("hasAuthority('UPDATE')")
+    public ResponseEntity<String> TestUpdate() {
+        return ResponseEntity.ok().body("You Can Update On This Page");
+    }
+
 }
