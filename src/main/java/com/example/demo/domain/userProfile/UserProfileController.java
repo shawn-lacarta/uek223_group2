0524215e12dbb069824ae70227afadc0bb062a22
nuweramohammad <mohammad@noseryoung.com>
2021-11-11 14:56:46 +0100

    deleted dead code

--- src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
+++ src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
@@ -14,35 +14,11 @@ public class UserProfileController {
 
     private final UserProfileService userProfileService;
 
-    @GetMapping("/read")
-    @PreAuthorize("hasAuthority('READ_ALL')")
-    public ResponseEntity<String> TestRead() {
-        return ResponseEntity.ok().body("You Can Read This Page");
-    }
-
-    @GetMapping("/create")
-    @PreAuthorize("hasAuthority('CREATE')")
-    public ResponseEntity<String> TestCreate() {
-        return ResponseEntity.ok().body("You Can Create On This Page");
-    }
-
     @PostMapping("/addUserProfile")
     @PreAuthorize("hasAuthority('CREATE')")
     public ResponseEntity<UserProfile> addUserProfile(@RequestBody NewUserProfile userProfile) throws InstanceAlreadyExistsException {
         return ResponseEntity.ok().body(userProfileService.addUserProfile(userProfile));
     }
 
-
-    @GetMapping("/delete")
-    @PreAuthorize("hasAuthority('DELETE')")
-    public ResponseEntity<String> TestDelete() {
-        return ResponseEntity.ok().body("You Can Delete On This Page");
-    }
-
-    @GetMapping("/update")
-    @PreAuthorize("hasAuthority('UPDATE_ALL')")
-    public ResponseEntity<String> TestUpdate() {
-        return ResponseEntity.ok().body("You Can Update On This Page");
-    }
 
 }
