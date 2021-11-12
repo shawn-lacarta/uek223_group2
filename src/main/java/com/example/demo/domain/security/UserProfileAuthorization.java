17f9ea78e68851ce2656c444dd69415f7ebac4a9
nuweramohammad <mohammad@noseryoung.com>
2021-11-12 16:44:25 +0100

    adding auhtorization config

---
+++ src/main/java/com/example/demo/domain/security/UserProfileAuthorization.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain.security;
+
+import com.example.demo.domain.appUser.UserRepository;
+import com.example.demo.domain.userProfile.UserProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.security.Principal;
+import java.util.UUID;
+
+@Component("userProfileSecurity")
+public class UserProfileAuthorization {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
+    public boolean hasUserId(UUID id, Principal currentUser){
+        UUID idCurrent = userProfileRepository.findUserProfileByUser(userRepository.findByUsername(currentUser.getName())).getId();
+        if(idCurrent.equals(id))
+            return true;
+        return false;
+    }
+
+}
