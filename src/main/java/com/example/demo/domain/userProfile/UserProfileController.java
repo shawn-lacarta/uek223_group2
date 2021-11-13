ceda704fde245af0a73bf03f40f1be7fc4cb634b
nuweramohammad <mohammad@noseryoung.com>
2021-11-13 19:36:09 +0100

    deleted unused imports

--- src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
+++ src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
@@ -1,22 +1,17 @@
 package com.example.demo.domain.userProfile;
 
-import com.example.demo.domain.appUser.User;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.management.InstanceAlreadyExistsException;
-import javax.management.InstanceNotFoundException;
 import java.security.Principal;
-import java.util.*;
+import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/userprofile")
--- src/main/java/com/example/demo/domain/userProfile/UserProfileService.java
+++ src/main/java/com/example/demo/domain/userProfile/UserProfileService.java
@@ -1,15 +1,11 @@
 package com.example.demo.domain.userProfile;
-
-
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import javax.management.InstanceAlreadyExistsException;
 import java.security.Principal;
-import java.util.List;
 import java.util.UUID;
+
 public interface UserProfileService {
     UserProfile findById(UUID id, Principal currentUser);
     ResponseEntity addUserProfile(NewUserProfile newUserProfile) throws InstanceAlreadyExistsException;
