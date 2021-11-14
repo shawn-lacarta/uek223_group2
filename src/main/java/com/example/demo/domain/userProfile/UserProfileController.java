33676c6449cc1c3b3abc0d2814a447a9bcd437d1
nuweramohammad <mohammad@noseryoung.com>
2021-11-14 10:10:26 +0100

    added log to endpoints

--- src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
+++ src/main/java/com/example/demo/domain/userProfile/UserProfileController.java
@@ -2,6 +2,8 @@ package com.example.demo.domain.userProfile;
 
 import com.example.demo.domain.appUser.User;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +25,7 @@ import java.util.*;
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
+    private Logger logger = LoggerFactory.getLogger(UserProfileController.class);
 
     @Autowired
     public UserProfileController(UserProfileService userProfileService){
@@ -32,30 +35,34 @@ public class UserProfileController {
     @PostMapping("/")
     @PreAuthorize("hasAuthority('CREATE')")
     public ResponseEntity<String> addUserProfile(@RequestBody NewUserProfile userProfile) throws InstanceAlreadyExistsException {
+        logger.trace("POST USERPROFILES ENDPOINT ACCESSED");
         return ResponseEntity.ok().body(userProfileService.addUserProfile(userProfile));
     }
     @GetMapping("/{id}")
     @PreAuthorize("@userProfileSecurity.hasUserId(#id, #currentUser) || hasAuthority('READ_ALL')")
     public ResponseEntity<UserProfile> getOwnUser(@PathVariable UUID id, Principal currentUser) throws NullPointerException {
+        logger.trace("GET ONE USERPROFILE ENDPOINT ACCESSED");
         return ResponseEntity.ok().body(this.userProfileService.findById(id, currentUser));
     }
 
     @GetMapping("/")
     @PreAuthorize("hasAuthority('READ_ALL')")
     public ResponseEntity<Collection<UserProfile>> getAllUser(Pageable page) {
+        logger.trace("GET ALL USERPROFILES ENDPOINT ACCESSED");
         return new ResponseEntity(userProfileService.findAllUsers(page), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("@userProfileSecurity.hasUserId(#id, #currentUser) || hasAuthority('UPDATE_ALL')")
     public ResponseEntity<UserProfile> updateUserProfile(@PathVariable UUID id, Principal currentUser, @RequestBody UserProfile userProfile){
+        logger.trace("PUT USERPROFILE ENDPOINT ACCESSED");
         return ResponseEntity.ok().body(userProfileService.updateUserProfile(userProfile, id, currentUser));
-
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('DELETE')")
     public ResponseEntity<String> deleteUserProfile(@PathVariable("id") UUID id) throws NullPointerException{
+        logger.trace("DELETE USERPROFILE ENDPOINT ACCESSED");
         return ResponseEntity.ok().body(userProfileService.deleteById(id));
     }
 
