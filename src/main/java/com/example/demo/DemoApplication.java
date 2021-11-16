a4f85cbabbb44bbad1fb2b26be195a8c9c5ab1c4
nuweramohammad <mohammad@noseryoung.com>
2021-11-16 18:56:11 +0100

    changed path of swagger documentation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,12 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @SpringBootApplication
@@ -27,8 +30,22 @@ class DemoApplication {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo"))
 				.paths(PathSelectors.any())
-				.build();
+				.build().apiInfo(apiDetails());
 	}
+
+	private ApiInfo apiDetails(){
+		return new ApiInfo(
+				"Userprofiles",
+				"This url leads you to the login page of our application",
+				"",
+				"",
+				new springfox.documentation.service.Contact("Nuwera Mohammad, Shawn Lacarta, Matijas Polazarov", "http://localhost:8080/userprofile/", ""),
+				"API",
+				"",
+				Collections.emptyList());
+	}
+
+
 }
